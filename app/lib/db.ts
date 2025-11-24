1177265f4c169afd86c87492b19f15199280f835
Mosessniboy <samsonmosessimbolon@gmail.com>
2025-11-24 15:57:00 +0700

    qq

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,19 +1,20 @@
 import postgres from 'postgres';
 
-// GANTI DARI POSTGRES_URL KE MY_DB_URL
-const connectionString = process.env.MY_DB_URL; 
+// Ambil connection string dari environment variable
+const connectionString = process.env.POSTGRES_URL;
 
 // --- DEBUGGING BLOCK ---
 if (!connectionString) {
   throw new Error(`
-    FATAL ERROR: MY_DB_URL tidak ditemukan!
-    Pastikan Anda sudah membuat variabel 'MY_DB_URL' di Vercel dan melakukan REDEPLOY.
+    FATAL ERROR: POSTGRES_URL tidak ditemukan!
+    Pastikan Anda sudah membuat variabel 'POSTGRES_URL' di Vercel dan melakukan REDEPLOY.
   `);
 }
 // -----------------------
 
-const sql = postgres(connectionString, { 
-  ssl: 'require' 
+// Inisialisasi koneksi
+const sql = postgres(connectionString, {
+  ssl: { rejectUnauthorized: false }, // biasanya perlu untuk Neon
 });
 
 export default sql;
